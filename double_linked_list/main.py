830e30e60401fb08d7c4146715bdc6bb16c0df18
Pavan Palve <pavan@Mac.lan>
2025-12-18 00:29:25 +0530

    feat: reorder and all other code is ready

--- double_linked_list/main.py
+++ double_linked_list/main.py
@@ -2,30 +2,40 @@ from double_linked_list.schemas import DoubleLinkedList, Node
 
 
 def main():
-    dll = DoubleLinkedList()
-    values = [100, 200, 400, 500, 600]
-    dll.from_list(values)
-    dll.print_linked_list()
-"""
-    n1 = Node(100)
-    n2 = Node(200)
-    n3 = Node(300)
-    n4 = Node(400)
+    dll1 = DoubleLinkedList()
+    values = "A C E".split(" ")
+    dll1.from_list(values)
 
-    n1.next = n2
-    n2.next = n3
-    n3.next = n1
-    n4.next = None
+    dll2 = DoubleLinkedList()
+    values = "B D".split(" ")
+    dll2.from_list(values)
 
-    n1.prev = None
-    n2.prev = n1
-    n3.prev = n2
-    n4.prev = n3
+    dll1.print_linked_list()
+    dll2.print_linked_list()
 
-    dll.head = n1
-    dll.tail = n4
-"""
+    dll1.merge(dll2, "alternate")
+    dll1.print_linked_list()
 
 
 if __name__ == "__main__":
     main()
+
+    """
+        n1 = Node(100)
+        n2 = Node(200)
+        n3 = Node(300)
+        n4 = Node(400)
+
+        n1.next = n2
+        n2.next = n3
+        n3.next = n1
+        n4.next = None
+
+        n1.prev = None
+        n2.prev = n1
+        n3.prev = n2
+        n4.prev = n3
+
+        dll.head = n1
+        dll.tail = n4
+    """
